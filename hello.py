37c9e9f8d2d0b1bf6c00704872273420d2bcec10
LorenzoVald <24covid@gmail.com>
2025-02-14 02:22:29 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-Print("Hello world!") 
-Print("Name: [Your Full Name]") 
-Print("Course: [Your Course Name]") 
-print("Interests in programming: [Your Interests]") 
+print("Hello world!")
+print("Name: [Your Full Name]")
+print("Course: [Your Course Name]")
+print("Interests in programming: [Your Interests]")
