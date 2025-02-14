293a2e1dfee2856f513f8ed99163be1f426c590e
LorenzoVald <24covid@gmail.com>
2025-02-14 18:15:22 +0800

    My first python program

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 Print("Hello world!") 
-Print("Name: [Your full name]") 
-Print("Course: [Your course name]") 
-print("Interests in Programming: [Your interests]") 
+Print("Name: [Your Full Name]") 
+Print("Course: [Your Course Name]") 
+print("Interests in Programming: [Your Interests]") 
